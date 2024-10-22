56a2752ff086567938092da99213f894d4706a27
chengdai <chengdai.dev@gmail.com>
2024-10-22 19:17:16 +0200

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel/serverless';
+
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
    vite: {
     plugins: [tailwindcss()],
   },
