9a7c40fff800fec3c5a8350461885fc4b58e0053
chengdai <chengdai.dev@gmail.com>
2025-01-06 17:15:00 +0100

    chore: update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
 
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 
 export default defineConfig({
