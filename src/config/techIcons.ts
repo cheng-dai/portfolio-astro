5742ee45391ae384af3b1e7fed98b779706fbb1e
Cheng Dai <chengdai.dev@gmail.com>
2025-11-10 20:26:33 +0100

    Update style and update CV.

--- src/config/techIcons.ts
+++ src/config/techIcons.ts
@@ -1,11 +1,12 @@
 export const techStackIcons = {
-  "React": "react",
+  React: "react",
   "Tailwind css": "tailwindcss-icon",
-  "Vercel": "vercel-icon",
+  Vercel: "vercel-icon",
   "Next.js": "nextjs-icon",
-  "PWA": "pwa",
-  "PostgreSQL": "postgresql",
+  PWA: "pwa",
+  PostgreSQL: "postgresql",
   "Google Analytics": "google-analytics",
   "Browser Extension": "chrome-web-store",
-
-}
+  Clerk: "clerk",
+  Supabase: "supabase",
+};
